e9c3e072673721fcdd83451b710af02a1a12354c
unknown <earthbit@protonmail.com>
2023-08-10 09:44:20 -0700

    implemented game logic

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const { Socket } = require('dgram')
+const path = require('path')
+
+app.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname, '../ui/index.html'))
+})
+
+io.on('connection', (socket) => {
+    console.log('new user connectd')
+
+    socket.on('move', (data)=> {
+        io.sockets.emit('move', data)
+    })
+})
+
+
+http.listen(3000,  () => {
+    console.log('App is running')
+})
+// '192.168.213.168'
