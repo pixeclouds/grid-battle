9167fb7e9e86dcbe5eb0d5c02fc86c09bfe526a2
pixeclouds <earthbit@protonmail.com>
2023-09-07 00:02:31 -1200

    config db

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,19 @@
 const Pool = require('pg').Pool
 
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'demo',
+//     password: 'postgres',
+//     port: 5432,
+// })
+
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'demo',
-    password: 'postgres',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 })
 
 pool.connect((err, client, release) => {
